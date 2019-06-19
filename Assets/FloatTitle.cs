a2897abe6ee2ca280faec7ed15ed86f23297a2f8
Jonathan Chaiyadi <jonathan.chaiyadi@live.com>
2019-06-20 03:10:01 +0900

    Prototype Finalize

---
+++ Assets/FloatTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatTitle : MonoBehaviour
+{ 
+    private float counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter > 360) counter = 0;
+        transform.position = new Vector2(transform.position.x, 2.0f * Mathf.Sin(counter*Mathf.Deg2Rad));
+        counter += 0.25f;
+    }
+}
